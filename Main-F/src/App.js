cffbb41a9d017eef82fb61417d55f6e9d0efd792
kishlay <kishlay141@gmail.com>
2025-04-29 22:33:37 +0530

    updated the app.js

--- Main-F/src/App.js
+++ Main-F/src/App.js
@@ -7,28 +7,28 @@ import Notes from "./components/data";
 import { useState } from "react";
 
 function App() {
-    const [notes, setNotes] = useState(Notes);
+  const [notes, setNotes] = useState(Notes);
 
-    function addNote(newNote) {
-        setNotes((prevNotes) => {
-            return [...prevNotes, newNote];
-        });
-    }
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
 
-    function deleteNote(noteID) {
-        setNotes((prevNotes) => {
-            return prevNotes.filter((_, index) => index !== noteID);
-        });
-    }
+  function deleteNote(noteID) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((_, index) => index !== noteID);
+    });
+  }
 
-    return (
-        <div className="flex flex-col min-h-screen">
-            <Header />
-            <CreateNote onAdd={addNote} />
-            <Note notes={notes} onDelete={deleteNote} />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <CreateNote onAdd={addNote} />
+      <Note notes={notes} onDelete={deleteNote} />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
