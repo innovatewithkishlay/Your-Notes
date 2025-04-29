ecc3251be6bba04da71580458c17de9ed0a5fd8d
kishlay <kishlay141@gmail.com>
2025-04-29 22:11:43 +0530

    updated the note.js with framer motion

--- Main-F/src/components/note.jsx
+++ Main-F/src/components/note.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import PropDisplay from "./propDisplay";
+import { motion } from "framer-motion";
 
 export default function Note(props) {
   return (
     <div className="flex-grow">
-      <div className="grid gap-2 md:gap-4 grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 ">
-        {props.notes.map((note, index) => {
-          return (
+      <div className="grid gap-2 md:gap-4 grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2">
+        {props.notes.map((note, index) => (
+          <motion.div
+            key={index}
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.3, delay: index * 0.1 }}
+          >
             <PropDisplay
               {...props}
-              key={index}
               id={index}
               title={note.title}
               content={note.content}
             />
-          );
-        })}
+          </motion.div>
+        ))}
       </div>
     </div>
   );
